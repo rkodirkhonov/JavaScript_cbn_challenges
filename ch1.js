65f64d95768fe97f261c1cbe928ec6509db39075
Rustamjon Kodirkhonov <68010191+rkodirkhonov@users.noreply.github.com>
2022-08-17 06:51:52 +0900

    Update ch1.js

--- ch1.js
+++ ch1.js
@@ -1,6 +1,6 @@
-// function createFunction() {
-//   function sayHello(){
-//     console.log('hello');
-//   }
-//   return sayHello;
-// }
+function createFunction() {
+  function sayHello(){
+    console.log('hello');
+  }
+  return sayHello;
+}
